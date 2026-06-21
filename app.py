a3c5450224c56680704fceef8537efc094d743f1
Redrouthu Deshma saikavalli <deshmasaikavalliredrouthu@gmail.com>
2026-06-21 14:37:10 +0530

    app.py

--- app.py
+++ app.py
@@ -375,5 +375,6 @@ def add_old():
     return redirect(url_for('view', date=expense_date))
 
 # ---------------- RUN ----------------
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
